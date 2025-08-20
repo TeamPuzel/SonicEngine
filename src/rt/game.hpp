2875ab2bd34d70e98fc34a4ac117d2833e4292c1
TeamPuzel <puffed-01.blips@icloud.com>
2025-08-20 19:53:41 +0200

    Revert "some work"
    This reverts commit 2e30793e1f47f63b9fd227f13f94e7a1a8ed1af0.

--- src/rt/game.hpp
+++ src/rt/game.hpp
@@ -512,8 +512,8 @@ namespace rt {
         auto renderer = SDL_CreateRenderer(window, nullptr);
         if (not renderer) {
             throw RunError {
-                .reason = RunError::Reason::CouldNotCreateRenderer,
-                .description = SDL_GetError(),
+                RunError::Reason::CouldNotCreateRenderer,
+                SDL_GetError(),
             };
         }
         // We can discard the error, it is inefficient not to use vsync but if a platform doesn't support it
